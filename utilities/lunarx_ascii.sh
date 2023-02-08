06305fce8330128f958b73b230a9334adbf55280
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-02-08 14:55:44 -0500

    Added ascii source art

---
+++ utilities/lunarx_ascii.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+echo -e "\033[1;97m---------------------------------------------------------------------------------------------------------------------" 
+echo -e "\033[1;97m                                                      Welcome!"
+echo -e "\033[1;97m                               Project - Lunar Autonomous Regolith Excavator [ LunAR-X ]"
+echo -e "\033[1;97m  " 
+echo -e "\x1B[01;93m          XXXXXXXXXXXXXXXXXXXX               XXXXXXXXXXXXXXXXXXXX\x1B[00;91m                   &&&&&& "
+echo -e "\x1B[01;93m           XXXXXXXXXXXXXXXXXXXX            XXXXXXXXXXXXXXXXXXXX\x1B[00;91m             &&&&&&&&&&&&&&&&&&&&&" 
+echo -e "\x1B[01;93m            XXXXXXXXXXXXXXXXXXXX         XXXXXXXXXXXXXXXXXXXXX\x1B[00;91m          &&&&&&&&&&&&&&&&&&&&&&&&&&&&& " 
+echo -e "\x1B[01;93m             XXXXXXXXXXXXXXXXXXXX       XXXXXXXXXXXXXXXXXXXX\x1B[00;91m          &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& " 
+echo -e "\x1B[01;93m              XXXXXXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXX\x1B[00;91m          &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+echo -e "\x1B[01;93m               XXXXXXXXXXXXXXXXXXXX  XXXXXXXXXXXXXXXXXXX\x1B[00;91m           &&&&&&&&&&&&&&&&&&    &&&&&&&&&&&&&&&&&"
+echo -e "\x1B[01;93m                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\x1B[00;91m           &&&&&&&&&&&&&&             &&&&&&&&&&&&&&"
+echo -e "\x1B[01;93m                 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\x1B[00;91m            &&&&&&&&&&&&&                 &&&&&&&&&&&&&"
+echo -e "\x1B[01;93m                  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\x1B[00;91m              &&&&&&&&&&&&                   &&&&    &&&&"
+echo -e "\x1B[01;93m                   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\x1B[00;91m               &&&&&&&&&&&&                   &&  &&&&  &&"
+echo -e "\033[1;97m          00        00    00  00    00      00      00000\x1B[00;91m        &&&&&&&&&&&&                     &&&&&&&&"
+echo -e "\033[1;97m          00        00    00  000   00     0000     00  00\x1B[00;91m       &&&&&&&&&&&&                     &&&&&&&&"
+echo -e "\033[1;97m          00        00    00  00 0  00    00  00    00000\x1B[00;91m        &&&&&&&&&&&&             &&&&&&   &&&&&&  &&&&&&&"
+echo -e "\033[1;97m          00        00    00  00  0 00   00000000   00  00\x1B[00;91m       &&&&&&&&&&&&              &&&&&&&&      &&&&&&&&"
+echo -e "\033[1;97m          00000000   000000   00   000  00      00  00   00\x1B[00;91m      &&&&&&&&&&&&               &&&&&&&&&&&&&&&&&&&&" 
+echo -e "\x1B[01;93m                  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\x1B[00;91m                 &&&&&&&&&&&&                  &&&&&&&&&&&&&&" 
+echo -e "\x1B[01;93m                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\x1B[00;91m                &&&&&&&&&&&&                   &&&&&&&&&&&&"
+echo -e "\x1B[01;93m              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\x1B[00;91m               &&&&&&&&&&&&                   &&&&&&&&&&&&" 
+echo -e "\x1B[01;93m            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\x1B[00;91m              &&&&&&&&&&&&                   &&&&&&&&&&&&"
+echo -e "\x1B[01;93m           XXXXXXXXXXXXXXXXXXX  XXXXXXXXXXXXXXXXXXXX\x1B[00;91m             &&&&&&&&&&&&                   &&&&&&&&&&&&" 
+echo -e "\x1B[01;93m         XXXXXXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXX\x1B[00;91m            &&&&&&&&&&&&                   &&&&&&&&&&&&"
+echo -e "\x1B[01;93m       XXXXXXXXXXXXXXXXXXXX       XXXXXXXXXXXXXXXXXXXX\x1B[00;91m           &&&&&&&&&&&&                   &&&&&&&&&&&&" 
+echo -e "\x1B[01;93m      XXXXXXXXXXXXXXXXXXXX         XXXXXXXXXXXXXXXXXXXX\x1B[00;91m          &&&&&&&&&&&&                   &&&&&&&&&&&&"
+echo -e "\x1B[01;93m    XXXXXXXXXXXXXXXXXXXX            XXXXXXXXXXXXXXXXXXXX\x1B[00;91m                    " 
+echo -e "\x1B[01;93m  XXXXXXXXXXXXXXXXXXXX               XXXXXXXXXXXXXXXXXXXX\x1B[00;91m                   THE ROBOTICS INSTITUTE"
+echo -e " "
+echo -e "\033[1;97m                                                • Reshape The Moon •"
+echo -e "\033[1;97m---------------------------------------------------------------------------------------------------------------------"
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
