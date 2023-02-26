78a86c6963b86d1b1e2e4dad2454dc00ea02900c
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-02-26 14:58:09 -0500

    Added param server launch file

---
+++ src/lx_autonomy/src/lx_bringup_autonomy/launch/param_server.launch.py
@@ -0,0 +1,24 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    rover_parameters = os.path.join(
+        get_package_share_directory('lx_bringup_autonomy'),
+        'config',
+        'params.yaml'
+    )
+
+    param_server_node = Node(
+        package='lx_bringup_autonomy',
+        executable='param_server_node',
+        name='param_server_node',
+        parameters=[rover_parameters]
+    )
+
+    ld.add_action(param_server_node)
+
+    return ld
