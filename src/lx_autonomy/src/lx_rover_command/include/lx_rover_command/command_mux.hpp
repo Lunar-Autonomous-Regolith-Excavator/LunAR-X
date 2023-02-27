61e3504f4b89486ea8ee072b58265b5be9766b1e
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-02-27 14:41:38 -0500

    Fixed spelling

--- src/lx_autonomy/src/lx_rover_command/include/lx_rover_command/command_mux.hpp
+++ src/lx_autonomy/src/lx_rover_command/include/lx_rover_command/command_mux.hpp
@@ -25,9 +25,9 @@ class CommandMux: public rclcpp::Node
         struct lock_struct rover_soft_lock_;
         OpModeEnum current_rover_op_mode_ = OpModeEnum::STANDBY;
         TaskModeEnum current_rover_task_mode_ = TaskModeEnum::IDLE;
-        float max_mob_lin_vel_ = 0.5; // Reflect any defaul param changes here 
-        float max_mob_ang_vel_ = 0.1; // Reflect any defaul param changes here
-        float max_drum_speed_ = 0.1;  // Reflect any defaul param changes here
+        float max_mob_lin_vel_ = 0.5; // Reflect any default param changes here 
+        float max_mob_ang_vel_ = 0.1; // Reflect any default param changes here
+        float max_drum_speed_ = 0.1;  // Reflect any default param changes here
         std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
         std::shared_ptr<rclcpp::ParameterCallbackHandle> mob_param_cb_handle_;
         std::shared_ptr<rclcpp::ParameterCallbackHandle> act_param_cb_handle_;
