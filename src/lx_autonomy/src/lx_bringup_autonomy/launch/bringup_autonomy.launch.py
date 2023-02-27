3a41a153a921cff28bf0e3e722a1e6829d910cef
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-02-27 16:43:31 -0500

    Launch file and drum_speed passthrough logic modified

--- src/lx_autonomy/src/lx_bringup_autonomy/launch/bringup_autonomy.launch.py
+++ src/lx_autonomy/src/lx_bringup_autonomy/launch/bringup_autonomy.launch.py
@@ -1,6 +1,8 @@
 import os
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
@@ -8,18 +10,18 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     bringup_autonomy_dir = get_package_share_directory('lx_bringup_autonomy')
-    param_server_launch = launch.actions.IncludeLaunchDescription(
-                          launch.launch_description_sources.PythonLaunchDescriptionSource(
+    param_server_launch = IncludeLaunchDescription(
+                          PythonLaunchDescriptionSource(
                                 bringup_autonomy_dir + '/launch/param_server.launch.py'))
     
     rover_command_dir = get_package_share_directory('lx_rover_command')
-    command_mux_launch = launch.actions.IncludeLaunchDescription(
-                         launch.launch_description_sources.PythonLaunchDescriptionSource(
+    command_mux_launch = IncludeLaunchDescription(
+                         PythonLaunchDescriptionSource(
                                 rover_command_dir + '/launch/command_mux.launch.py'))
     
     external_interface_dir = get_package_share_directory('lx_external_interface')
-    external_interface_launch = launch.actions.IncludeLaunchDescription(
-                                launch.launch_description_sources.PythonLaunchDescriptionSource(
+    external_interface_launch = IncludeLaunchDescription(
+                                PythonLaunchDescriptionSource(
                                     external_interface_dir + '/launch/external_interface.launch.py'))
 
     ld.add_action(param_server_launch)
