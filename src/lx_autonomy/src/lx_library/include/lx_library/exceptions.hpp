3d4a8b2009f3c100db8d6b1dc59d1f4fdcf324e8
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-03-01 11:38:26 -0500

    Added exceptions header with placeholder function

---
+++ src/lx_autonomy/src/lx_library/include/lx_library/exceptions.hpp
@@ -0,0 +1,17 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+namespace LX
+{
+    class SubTaskException{
+        // TODO
+    };
+
+    class TaskException{
+        // TODO
+    };
+}
+
+void placeHolderFunction();
+
+#endif
