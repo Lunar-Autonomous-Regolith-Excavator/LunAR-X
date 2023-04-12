4f9793f692e10dfad857f7a0aa02c841483ffd2d
Vibhakar Mohta <vib2810@gmail.com>
2023-04-12 16:49:30 -0400

    added cameras and imu to lx_bringup_hardware

--- src/lx_hardware/ros2_ws/src/lx_packages/lx_bringup_hardware/launch/bringup_hardware.launch.py
+++ src/lx_hardware/ros2_ws/src/lx_packages/lx_bringup_hardware/launch/bringup_hardware.launch.py
@@ -1,4 +1,5 @@
 import os
+import launch
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
@@ -7,21 +8,39 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    realsense_params = os.path.join(
+            get_package_share_directory('lx_bringup_hardware'),
+            'params',
+            'realsense.yaml'
+        )
+    
     ld = LaunchDescription()
 
     # hardware_mux launch
     hardware_mux_dir = get_package_share_directory('lx_hardware_mux')
     hardware_mux_launch = IncludeLaunchDescription(
-                         PythonLaunchDescriptionSource(
-                                hardware_mux_dir + '/launch/hardware_mux.launch.py'))
+                          PythonLaunchDescriptionSource(
+                          hardware_mux_dir + '/launch/hardware_mux.launch.py'))
     
     # status_relay launch
     status_relay_dir = get_package_share_directory('lx_status_relay')
     status_relay_launch = IncludeLaunchDescription(
-                                PythonLaunchDescriptionSource(
-                                    status_relay_dir + '/launch/status_relay.launch.py'))
+                          PythonLaunchDescriptionSource(
+                          status_relay_dir + '/launch/status_relay.launch.py'))
+
+    imu_node = Node(package='imu', executable='imu_node', name='imu_node')
+
+    realsense_node = Node(    
+      package='robot_localization',
+      executable='ekf_node',
+      name='ekf_global_node',
+      #load yaml and set use_sim_time params
+      parameters=[realsense_params],
+    ),
 
     ld.add_action(hardware_mux_launch)
     ld.add_action(status_relay_launch)
+    ld.add_action(imu_node)
+    ld.add_action(realsense_node)
 
     return ld
