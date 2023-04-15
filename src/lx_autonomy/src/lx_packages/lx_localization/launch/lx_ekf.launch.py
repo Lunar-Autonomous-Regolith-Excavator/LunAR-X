8dc4581decb3f8ff3e85f137aad8aa2450416f74
Vibhakar Mohta <vib2810@gmail.com>
2023-04-15 01:35:06 -0400

    localization integrated, working on bag file

--- src/lx_autonomy/src/lx_packages/lx_localization/launch/lx_ekf.launch.py
+++ src/lx_autonomy/src/lx_packages/lx_localization/launch/lx_ekf.launch.py
@@ -15,44 +15,68 @@ def generate_launch_description():
     'params',
     'lx_ts_localization_params.yaml'
   )
+  # set use_sim_time param
+  use_sim_time_param = launch.substitutions.LaunchConfiguration('use_sim_time', default='false')
 
   return LaunchDescription([
-    # Node(
-    #   package='robot_localization',
-    #   executable='ekf_node',
-    #   name='ekf_odom_node',
-    #   #load yaml and set use_sim_time params
-    #   parameters=[lx_localization_params, {'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time', default='true')}],
-    # ),
-    # Node(    
-    #   package='robot_localization',
-    #   executable='ekf_node',
-    #   name='ekf_global_node',
-    #   #load yaml and set use_sim_time params
-    #   parameters=[lx_ts_localization_params, {'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time', default='true')}],
-    # ),
-    # Node(
-    #   package='lx_localization',
-    #   executable='remap_husky_odom',
-    #   name='remap_husky_odom',
-    #   output='screen'
-    # ),
+    Node(
+      package='robot_localization',
+      executable='ekf_node',
+      name='ekf_odom_node',
+      #load yaml and set use_sim_time params
+      parameters=[lx_localization_params, {'use_sim_time': use_sim_time_param}],
+      remappings=[
+        ('odometry/filtered', 'odometry/ekf_odom_node')
+      ]
+    ),
+
+    Node(    
+      package='robot_localization',
+      executable='ekf_node',
+      name='ekf_global_node',
+      #load yaml and set use_sim_time params
+      parameters=[lx_ts_localization_params, {'use_sim_time': use_sim_time_param}],
+      remappings=[
+        ('odometry/filtered', 'odometry/ekf_global_node')
+      ]
+    ),
+  
+    Node(
+      package='lx_localization',
+      executable='remap_msgs_localization',
+      name='remap_msgs_localization',
+      output='screen',
+      parameters=[{'use_sim_time': use_sim_time_param}],
+    ),
+
     # static tf transform from base_link to imu_link
-    # Node(
-    #   package='tf2_ros',
-    #   executable='static_transform_publisher',
-    #   name='base_link_to_imu_link',
-    #   output='screen',
-    #   arguments=['0.1', '0', '0.25', '1.57', '0', '3.1415', 'base_link', 'vectornav']
-    # ), # (x y z yaw pitch roll frame_id child_frame_id period_in_ms)
+    Node(
+      package='tf2_ros',
+      executable='static_transform_publisher',
+      name='base_link_to_imu_link',
+      output='screen',
+      arguments=['0.1', '0', '0.5', '3.1415', '0', '3.1415', 'base_link', 'vectornav'],
+      parameters=[{'use_sim_time': use_sim_time_param}]
+    ), # (x y z yaw pitch roll frame_id child_frame_id period_in_ms)
+
+    # static tf transform from base_link to total_station_prism
+    Node(
+      package='tf2_ros',
+      executable='static_transform_publisher',
+      name='base_link_to_total_station',
+      output='screen',
+      arguments=['0.2', '-0.3', '0.6', '0', '0', '0', 'base_link', 'total_station_prism'],
+      parameters=[{'use_sim_time': use_sim_time_param}]
+    ), # (x y z yaw pitch roll frame_id child_frame_id period_in_ms)
+
     # rviz2 node with config file and set use_sim_time params
     Node(
       package='rviz2',
       executable='rviz2',
       name='rviz2',
       output='screen',
-      arguments=['-d', "/home/lx_autonomy/lx_autonomy_ws/bags/localization.rviz"],
-      parameters=[{'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time', default='true')}]
+      arguments=['-d', "/home/lx_autonomy/lx_autonomy_ws/src/lx_packages/lx_localization/config/localization.rviz"],
+      parameters=[{'use_sim_time': use_sim_time_param}]
     )      
-
+    
   ])
