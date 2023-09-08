d79e7d6873d4ad811299c3235371d10a10aeeeb5
lx-xavier2 <lunarx.cmu@gmail.com>
2023-09-08 19:14:55 -0400

    added relays for tool info and tool raw info

--- src/lx_autonomy/src/lx_packages/lx_perception/include/lx_perception/pcl_relay.hpp
+++ src/lx_autonomy/src/lx_packages/lx_perception/include/lx_perception/pcl_relay.hpp
@@ -4,20 +4,30 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
+#include "lx_msgs/msg/tool_info.hpp"
 
 class PCLRelay: public rclcpp::Node
 {
     private:
         rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pcl_subscriber_;
-        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr relay_publisher_;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pcl_publisher_;
 
         rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr cam_subscriber_;
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr cam_publisher_;
+
+        rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr tool_raw_info_subscriber_;
+        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr tool_raw_info_publisher_;
+
+        rclcpp::Subscription<lx_msgs::msg::ToolInfo>::SharedPtr tool_info_subscriber_;
+        rclcpp::Publisher<lx_msgs::msg::ToolInfo>::SharedPtr tool_info_publisher_;
 
         void setupCommunications();
 
         void pclCallBack(const sensor_msgs::msg::PointCloud2::SharedPtr );
         void camCallBack(const sensor_msgs::msg::Image::SharedPtr );
+        void toolRawInfoCallBack(const std_msgs::msg::Float64MultiArray::SharedPtr );
+        void toolInfoCallBack(const lx_msgs::msg::ToolInfo::SharedPtr );
 
     public:
         PCLRelay();
--- src/lx_autonomy/src/lx_packages/lx_perception/src/pcl_relay.cpp
+++ src/lx_autonomy/src/lx_packages/lx_perception/src/pcl_relay.cpp
@@ -8,41 +8,42 @@ PCLRelay::PCLRelay(): Node("pcl_relay_node"){
 }
 
 void PCLRelay::setupCommunications(){
-    pcl_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("/camera/depth/color/points", 1 , 
-                            std::bind(&PCLRelay::pclCallBack, this, std::placeholders::_1));
-    
-    relay_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("pcl_relay", 1);
-
     // Set the QoS settings best effort
     rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
 
-
-    // Create the subscriber
+    // Subscribers
+    pcl_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("/camera/depth/color/points", 1 , 
+                            std::bind(&PCLRelay::pclCallBack, this, std::placeholders::_1));
+    
     cam_subscriber_ = this->create_subscription<sensor_msgs::msg::Image>(
       "/camera/color/image_raw", qos, std::bind(&PCLRelay::camCallBack, this, std::placeholders::_1));
-
-    // Create the publisher
+    
+    tool_raw_info_subscriber_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+      "/tool_raw_info", qos, std::bind(&PCLRelay::toolRawInfoCallBack, this, std::placeholders::_1));
+    
+    tool_info_subscriber_ = this->create_subscription<lx_msgs::msg::ToolInfo>(
+      "/tool_info", qos, std::bind(&PCLRelay::toolInfoCallBack, this, std::placeholders::_1));
+    
+    // Publishers
+    pcl_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("pcl_relay", qos);
     cam_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("cam_relay", qos);
+    tool_raw_info_publisher_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("tool_raw_info_relay", qos);
+    tool_info_publisher_ = this->create_publisher<lx_msgs::msg::ToolInfo>("tool_info_relay", qos);
 }
 
-void PCLRelay::pclCallBack(const sensor_msgs::msg::PointCloud2::SharedPtr pcl_rcvd_msg){
-    auto pcl_pub_msg = sensor_msgs::msg::PointCloud2();
-    
-    pcl_pub_msg.header.frame_id = pcl_rcvd_msg->header.frame_id;
-    pcl_pub_msg.header.stamp = pcl_rcvd_msg->header.stamp;
-    pcl_pub_msg.height = pcl_rcvd_msg->height;
-    pcl_pub_msg.width = pcl_rcvd_msg->width;
-    pcl_pub_msg.fields = pcl_rcvd_msg->fields;
-    pcl_pub_msg.is_bigendian = pcl_rcvd_msg->is_bigendian;
-    pcl_pub_msg.point_step = pcl_rcvd_msg->point_step;
-    pcl_pub_msg.row_step = pcl_rcvd_msg->row_step;
-    pcl_pub_msg.data = pcl_rcvd_msg->data;
-    pcl_pub_msg.is_dense = pcl_rcvd_msg->is_dense;
 
-    relay_publisher_->publish(pcl_pub_msg);
+void PCLRelay::pclCallBack(const sensor_msgs::msg::PointCloud2::SharedPtr pcl_rcvd_msg){
+    pcl_publisher_->publish(*pcl_rcvd_msg);
 }
 
 void PCLRelay::camCallBack(const sensor_msgs::msg::Image::SharedPtr cam_rcvd_msg){
-    // print some info about the image
     cam_publisher_->publish(*cam_rcvd_msg);
 }   
+
+void PCLRelay::toolRawInfoCallBack(const std_msgs::msg::Float64MultiArray::SharedPtr tool_raw_info_rcvd_msg){
+    tool_raw_info_publisher_->publish(*tool_raw_info_rcvd_msg);
+}
+
+void PCLRelay::toolInfoCallBack(const lx_msgs::msg::ToolInfo::SharedPtr tool_info_rcvd_msg){
+    tool_info_publisher_->publish(*tool_info_rcvd_msg);
+}
