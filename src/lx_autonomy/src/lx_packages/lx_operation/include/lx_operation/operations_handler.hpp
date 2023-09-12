e597093c840d394cc42d399344d7c2ea93f10846
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-09-12 19:16:06 -0400

    Added operation action functions

--- src/lx_autonomy/src/lx_packages/lx_operation/include/lx_operation/operations_handler.hpp
+++ src/lx_autonomy/src/lx_packages/lx_operation/include/lx_operation/operations_handler.hpp
@@ -2,26 +2,46 @@
 #define OPERATIONS_HANDLER_H
 
 #include <rclcpp/rclcpp.hpp>
-#include "lx_library/task.hpp"
 #include <queue>
 #include <list>
 #include <memory>
+#include <functional>
+#include <thread>
+#include "lx_library/task.hpp"
+#include "geometry_msgs/msg/point.msg"
+#include "lx_msgs/msg/berm_config.msg"
+#include "lx_msgs/action/operation.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+
 
 class OperationsHandler: public rclcpp::Node
 {
     private:
+        using Operation = lx_msgs::action::Operation;
+        using GoalHandleOperation = rclcpp_action::ServerGoalHandle<Operation>;
+
         std::queue<std::shared_ptr<Task>, std::list<std::shared_ptr<Task>>> task_queue_ {};
+        lx_msgs::msg::BermConfig berm_config_;
 
         // Action server
+        rclcpp_action::Server<Operation>::SharedPtr operation_action_server_;
 
         // Functions
+        void setupCommunications();
+        rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID& , std::shared_ptr<const Operation::Goal> );
+        rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleOperation> );
+        void handle_accepted(const std::shared_ptr<GoalHandleOperation> );
+        void executeOperation(const std::shared_ptr<GoalHandleOperation> );
         std::queue<std::shared_ptr<Task>, std::list<std::shared_ptr<Task>>> planner();
-
+        bool checkBermBuilt();
         bool executeTaskQueue();
 
     public:
         OperationsHandler();
         ~OperationsHandler(){}
 };
+
+RCLCPP_COMPONENTS_REGISTER_NODE(OperationsHandler)
 
 #endif
--- src/lx_autonomy/src/lx_packages/lx_operation/src/operations_handler.cpp
+++ src/lx_autonomy/src/lx_packages/lx_operation/src/operations_handler.cpp
@@ -9,15 +9,94 @@
  * - Summary
  * 
  * TODO
- * - Add todos
+ * - Check operation action
+ * - Write planner
+ * - Write executeTaskQueue
+ * - Write checkBermBuilt
+ * - Finish executeOperation
  * */
 
 #include "lx_operation/operations_handler.hpp"
 
 OperationsHandler::OperationsHandler(): Node("operations_handler_node"){
+    setupCommunications();
+    RCLCPP_INFO(this->get_logger(), "Operations handler initialized");
+}
 
+void OperationsHandler::setupCommunications(){
+    // Action server
+    using namespace std::placeholders;
+    this->operation_action_server_ = rclcpp_action::create_server<Operation>(this, "operations_action",
+                                            std::bind(&OperationsHandler::::handle_goal, this, _1, _2),
+                                            std::bind(&OperationsHandler::::handle_cancel, this, _1),
+                                            std::bind(&OperationsHandler::::handle_accepted, this, _1));
+}
+
+rclcpp_action::GoalResponse OperationsHandler::handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const Operation::Goal> goal){
+    berm_config_.berm_configuration = goal->requested_berm_config.berm_configuration;
+    RCLCPP_INFO(this->get_logger(), "Received berm goal request with configuration:");
+    for(auto &berm_node: berm_config_.berm_configuration){
+        RCLCPP_INFO(this->get_logger(), "%f, %f", berm_node.x, berm_node.y);
+    }
+    (void)uuid;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse OperationsHandler::handle_cancel(const std::shared_ptr<GoalHandleOperation> goal_handle){
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+    (void)goal_handle;
+    return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void OperationsHandler::handle_accepted(const std::shared_ptr<GoalHandleOperation> goal_handle){
+    using namespace std::placeholders;
+    std::thread{std::bind(&OperationsHandler::executeOperation, this, _1), goal_handle}.detach();
+}
+
+void OperationsHandler::executeOperation(const std::shared_ptr<GoalHandleOperation> goal_handle){
+    RCLCPP_INFO(this->get_logger(), "Executing operation goal");
+
+    do{
+        // Call planner to plan full path
+        // Get task queue from planner
+        task_queue_ = planner();
+
+        // Only keep first N tasks in queue
+
+        // Execute task queue
+        if(!executeTaskQueue()){
+            // If any task fails, return goal failure
+        }
+        
+        // If all tasks successful, continue next loop iteration
+    } 
+    while(!checkBermBuilt());
+    
+    // If berm is built, return goal success
+
+    if (rclcpp::ok()) {
+      result->success = true;
+      goal_handle->succeed(result);
+      RCLCPP_INFO(this->get_logger(), "Operations goal succeeded, berm built");
+    }
 }
 
 std::queue<std::shared_ptr<Task>, std::list<std::shared_ptr<Task>>> OperationsHandler::planner(){
+    // TODO
 
+    // Decide planner inputs
+}
+
+bool OperationsHandler::executeTaskQueue(){
+    // TODO
+
+    // Execute task queue
+}
+
+bool checkBermBuilt(){
+    // Call berm evaluation service
+
+    // Send action feedback on progress
+
+    // Return true if berm is built
 }
