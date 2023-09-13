49e23d7de40199e9c404bc5781ab5b98665a14dc
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-09-13 11:25:36 -0400

    Added parameter fetching to operations handler

--- src/lx_autonomy/src/lx_packages/lx_operation/include/lx_operation/operations_handler.hpp
+++ src/lx_autonomy/src/lx_packages/lx_operation/include/lx_operation/operations_handler.hpp
@@ -11,6 +11,8 @@
 #include "geometry_msgs/msg/point.hpp"
 #include "lx_msgs/msg/berm_config.hpp"
 #include "lx_msgs/action/operation.hpp"
+#include "rcl_interfaces/srv/get_parameters.hpp"
+#include "rcl_interfaces/msg/parameter.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 
@@ -18,18 +20,48 @@
 class OperationsHandler: public rclcpp::Node
 {
     private:
+        // Variables & pointers -----------------
         using Operation = lx_msgs::action::Operation;
         using GoalHandleOperation = rclcpp_action::ServerGoalHandle<Operation>;
-        std::vector<unsigned int> executed_task_ids_ {};
-
         std::queue<std::shared_ptr<Task>, std::list<std::shared_ptr<Task>>> task_queue_ {};
         lx_msgs::msg::BermConfig berm_config_;
-
+        std::vector<unsigned int> executed_task_ids_ {};
+        // Service clients
+        rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedPtr get_params_client_;
         // Action server
         rclcpp_action::Server<Operation>::SharedPtr operation_action_server_;
+        // Parameter handling
+        struct lock_struct rover_soft_lock_;
+        OpModeEnum current_rover_op_mode_ = OpModeEnum::STANDBY;
+        TaskModeEnum current_rover_task_mode_ = TaskModeEnum::IDLE;
+        std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
+        std::shared_ptr<rclcpp::ParameterCallbackHandle> mob_param_cb_handle_;
+        std::shared_ptr<rclcpp::ParameterCallbackHandle> act_param_cb_handle_;
+        std::shared_ptr<rclcpp::ParameterCallbackHandle> op_mode_param_cb_handle_;
+        std::shared_ptr<rclcpp::ParameterCallbackHandle> task_mode_param_cb_handle_;
+        // --------------------------------------
 
         // Functions
+        /*
+        * Set up subscribers, publishers, clients & servers of the node
+        * */
         void setupCommunications();
+
+        /*
+        * Set up tracking of global parameters
+        * */
+        void setupParams();
+
+        /*
+        * Get starting values of global parameters
+        * */
+        void getParams();
+
+        /*
+        * Callback function for starting values of global parameters
+        * */
+        void paramCB(rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedFuture );
+
         rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID& , std::shared_ptr<const Operation::Goal> );
         rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleOperation> );
         void handle_accepted(const std::shared_ptr<GoalHandleOperation> );
--- src/lx_autonomy/src/lx_packages/lx_operation/src/operations_handler.cpp
+++ src/lx_autonomy/src/lx_packages/lx_operation/src/operations_handler.cpp
@@ -28,8 +28,80 @@ OperationsHandler::OperationsHandler(const rclcpp::NodeOptions& options = rclcpp
     // Set up subscriptions, publishers, services, action servers and clients
     setupCommunications();
 
+    // Get parameters from the global parameter server
+    getParams();
+
+    // Set up parameters from the global parameter server
+    setupParams();
+
 
     RCLCPP_INFO(this->get_logger(), "Operations handler initialized");
+}
+
+void OperationsHandler::getParams(){
+    while(!get_params_client_->wait_for_service(std::chrono::seconds(2))){
+      RCLCPP_INFO(this->get_logger(), "Could not contact param server");
+      return;
+    }
+
+    // Get important parameters
+    auto get_request = std::make_shared<rcl_interfaces::srv::GetParameters::Request>();
+    get_request->names = {"rover.mobility_lock", "rover.actuation_lock", 
+                          "rover.op_mode", "rover.task_mode"};
+
+    // Send request
+    auto param_result_ = get_params_client_->async_send_request(get_request,std::bind(&OperationsHandler::paramCB, this, std::placeholders::_1));
+}
+
+void OperationsHandler::paramCB(rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedFuture future){
+    auto status = future.wait_for(std::chrono::milliseconds(100));
+    // If request successful, save all params in global variables
+    if (status == std::future_status::ready) {
+        // params_timer_ = this->create_wall_timer(std::chrono::seconds(10), 
+        //                     std::bind(&LXGUIBackend::getParameters, this));
+        
+        rover_soft_lock_.mobility_lock = future.get()->values.at(0).bool_value;
+        RCLCPP_INFO(this->get_logger(), "Parameter set Mobility: %s", (rover_soft_lock_.mobility_lock?"Locked":"Unlocked"));
+        rover_soft_lock_.actuation_lock = future.get()->values.at(1).bool_value;
+        RCLCPP_INFO(this->get_logger(), "Parameter set Actuation: %s", (rover_soft_lock_.actuation_lock?"Locked":"Unlocked"));
+
+        switch(future.get()->values.at(2).integer_value){
+            case 0:
+               current_rover_op_mode_ = OpModeEnum::STANDBY;
+               RCLCPP_INFO(this->get_logger(), "Parameter set Operation mode: Standby");
+               break;
+            case 1:
+               current_rover_op_mode_ = OpModeEnum::TELEOP;
+               RCLCPP_INFO(this->get_logger(), "Parameter set Operation mode: Teleop");
+               break;
+            case 2:
+               current_rover_op_mode_ = OpModeEnum::AUTONOMOUS;
+               RCLCPP_INFO(this->get_logger(), "Parameter set Operation mode: Autonomous");
+               break;
+        }
+
+        switch(future.get()->values.at(3).integer_value){
+            case 0:
+               current_rover_task_mode_ = TaskModeEnum::IDLE;
+               RCLCPP_INFO(this->get_logger(), "Parameter set Task mode: Idle");
+               break;
+            case 1:
+               current_rover_task_mode_ = TaskModeEnum::NAV;
+               RCLCPP_INFO(this->get_logger(), "Parameter set Task mode: Navigation");
+               break;
+            case 2:
+               current_rover_task_mode_ = TaskModeEnum::EXC;
+               RCLCPP_INFO(this->get_logger(), "Parameter set Task mode: Excavation");
+               break;
+            case 3:
+               current_rover_task_mode_ = TaskModeEnum::DMP;
+               RCLCPP_INFO(this->get_logger(), "Parameter set Task mode: Dumping");
+               break;
+        }
+    } 
+    else {
+        RCLCPP_INFO(this->get_logger(), "Service In-Progress...");
+    }
 }
 
 void OperationsHandler::setupCommunications(){
@@ -41,6 +113,70 @@ void OperationsHandler::setupCommunications(){
                                             std::bind(&OperationsHandler::handle_goal, this, _1, _2),
                                             std::bind(&OperationsHandler::handle_cancel, this, _1),
                                             std::bind(&OperationsHandler::handle_accepted, this, _1));
+}
+
+void OperationsHandler::setupParams(){
+    // Subscriber for global parameter events
+    param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
+
+    // Callback Lambdas
+    auto mob_params_callback = [this](const rclcpp::Parameter & p) {
+        RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": %s", p.get_name().c_str(), (p.as_bool()?"Locked":"Unlocked"));
+        rover_soft_lock_.mobility_lock = p.as_bool();
+    };
+    auto act_params_callback = [this](const rclcpp::Parameter & p) {
+        RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": %s", p.get_name().c_str(), (p.as_bool()?"Locked":"Unlocked"));
+        rover_soft_lock_.actuation_lock = p.as_bool();
+    };
+    auto op_mode_params_callback = [this](const rclcpp::Parameter & p) {
+        switch(p.as_int()){
+            case 0:
+               current_rover_op_mode_ = OpModeEnum::STANDBY;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Standby", p.get_name().c_str());
+               break;
+            case 1:
+               current_rover_op_mode_ = OpModeEnum::TELEOP;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Teleop", p.get_name().c_str());
+               break;
+            case 2:
+               current_rover_op_mode_ = OpModeEnum::AUTONOMOUS;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Autonomous", p.get_name().c_str());
+               break;
+        }
+    };
+    auto task_mode_params_callback = [this](const rclcpp::Parameter & p){
+        switch(p.as_int()){
+            case 0:
+               current_rover_task_mode_ = TaskModeEnum::IDLE;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Idle", p.get_name().c_str());
+               break;
+            case 1:
+               current_rover_task_mode_ = TaskModeEnum::NAV;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Navigation", p.get_name().c_str());
+               break;
+            case 2:
+               current_rover_task_mode_ = TaskModeEnum::EXC;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Excavation", p.get_name().c_str());
+               break;
+            case 3:
+               current_rover_task_mode_ = TaskModeEnum::DMP;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Dumping", p.get_name().c_str());
+               break;
+        }
+    };
+
+    // Names of node & params for adding callback
+    auto param_server_name = std::string("param_server_node");
+    auto mob_lock_param_name = std::string("rover.mobility_lock");
+    auto act_lock_param_name = std::string("rover.actuation_lock");
+    auto op_mode_param_name = std::string("rover.op_mode");
+    auto task_mode_param_name = std::string("rover.task_mode");
+
+    // Store callback handles for each parameter
+    mob_param_cb_handle_ = param_subscriber_->add_parameter_callback(mob_lock_param_name, mob_params_callback, param_server_name);
+    act_param_cb_handle_ = param_subscriber_->add_parameter_callback(act_lock_param_name, act_params_callback, param_server_name);
+    op_mode_param_cb_handle_ = param_subscriber_->add_parameter_callback(op_mode_param_name, op_mode_params_callback, param_server_name);
+    task_mode_param_cb_handle_ = param_subscriber_->add_parameter_callback(task_mode_param_name, task_mode_params_callback, param_server_name);
 }
 
 rclcpp_action::GoalResponse OperationsHandler::handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const Operation::Goal> goal){
