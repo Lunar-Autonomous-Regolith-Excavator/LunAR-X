85203c16a35340d78e0368f25f245f114e08d574
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-09-13 18:41:03 -0400

    Added callbacks to auto- actions

--- src/lx_autonomy/src/lx_packages/lx_operation/src/auto_dig_handler.cpp
+++ src/lx_autonomy/src/lx_packages/lx_operation/src/auto_dig_handler.cpp
@@ -93,3 +93,125 @@ void AutoDigHandler::paramCB(rclcpp::Client<rcl_interfaces::srv::GetParameters>:
         RCLCPP_INFO(this->get_logger(), "Service In-Progress...");
     }
 }
+
+void AutoDigHandler::setupCommunications(){
+    // Add all subscriptions, publishers and services here
+
+    // Clients
+    get_params_client_ = this->create_client<rcl_interfaces::srv::GetParameters>("/param_server_node/get_parameters");
+    // Action server
+    using namespace std::placeholders;
+    this->autodig_action_server_ = rclcpp_action::create_server<AutoDig>(this, "operations/autodig_action",
+                                            std::bind(&AutoDigHandler::handle_goal, this, _1, _2),
+                                            std::bind(&AutoDigHandler::handle_cancel, this, _1),
+                                            std::bind(&AutoDigHandler::handle_accepted, this, _1));
+}
+
+void AutoDigHandler::setupParams(){
+    // Subscriber for global parameter events
+    param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
+
+    // Callback Lambdas
+    auto mob_params_callback = [this](const rclcpp::Parameter & p) {
+        RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": %s", p.get_name().c_str(), (p.as_bool()?"Locked":"Unlocked"));
+        rover_soft_lock_.mobility_lock = p.as_bool();
+    };
+    auto act_params_callback = [this](const rclcpp::Parameter & p) {
+        RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": %s", p.get_name().c_str(), (p.as_bool()?"Locked":"Unlocked"));
+        rover_soft_lock_.actuation_lock = p.as_bool();
+    };
+    auto op_mode_params_callback = [this](const rclcpp::Parameter & p) {
+        switch(p.as_int()){
+            case 0:
+               current_rover_op_mode_ = OpModeEnum::STANDBY;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Standby", p.get_name().c_str());
+               break;
+            case 1:
+               current_rover_op_mode_ = OpModeEnum::TELEOP;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Teleop", p.get_name().c_str());
+               break;
+            case 2:
+               current_rover_op_mode_ = OpModeEnum::AUTONOMOUS;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Autonomous", p.get_name().c_str());
+               break;
+        }
+    };
+    auto task_mode_params_callback = [this](const rclcpp::Parameter & p){
+        switch(p.as_int()){
+            case 0:
+               current_rover_task_mode_ = TaskModeEnum::IDLE;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Idle", p.get_name().c_str());
+               break;
+            case 1:
+               current_rover_task_mode_ = TaskModeEnum::NAV;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Navigation", p.get_name().c_str());
+               break;
+            case 2:
+               current_rover_task_mode_ = TaskModeEnum::EXC;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Excavation", p.get_name().c_str());
+               break;
+            case 3:
+               current_rover_task_mode_ = TaskModeEnum::DMP;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Dumping", p.get_name().c_str());
+               break;
+        }
+    };
+
+    // Names of node & params for adding callback
+    auto param_server_name = std::string("param_server_node");
+    auto mob_lock_param_name = std::string("rover.mobility_lock");
+    auto act_lock_param_name = std::string("rover.actuation_lock");
+    auto op_mode_param_name = std::string("rover.op_mode");
+    auto task_mode_param_name = std::string("rover.task_mode");
+
+    // Store callback handles for each parameter
+    mob_param_cb_handle_ = param_subscriber_->add_parameter_callback(mob_lock_param_name, mob_params_callback, param_server_name);
+    act_param_cb_handle_ = param_subscriber_->add_parameter_callback(act_lock_param_name, act_params_callback, param_server_name);
+    op_mode_param_cb_handle_ = param_subscriber_->add_parameter_callback(op_mode_param_name, op_mode_params_callback, param_server_name);
+    task_mode_param_cb_handle_ = param_subscriber_->add_parameter_callback(task_mode_param_name, task_mode_params_callback, param_server_name);
+}
+
+rclcpp_action::GoalResponse AutoDigHandler::handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const AutoDig::Goal> goal){
+    RCLCPP_INFO(this->get_logger(), "Received autodig request");
+    (void)uuid;
+    
+    // Accept and execute action
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse AutoDigHandler::handle_cancel(const std::shared_ptr<GoalHandleAutoDig> goal_handle){
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+    (void)goal_handle;
+    
+    // Cancel action
+    return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void AutoDigHandler::handle_accepted(const std::shared_ptr<GoalHandleAutoDig> goal_handle){
+    using namespace std::placeholders;
+    
+    // Start thread to execute action and detach
+    std::thread{std::bind(&AutoDigHandler::executeAutoDig, this, _1), goal_handle}.detach();
+}
+
+void AutoDigHandler::executeAutoDig(const std::shared_ptr<GoalHandleAutoDig> goal_handle){
+    RCLCPP_INFO(this->get_logger(), "Executing autodig request");
+    
+    // Get goal, feedback and result
+    const auto goal = goal_handle->get_goal();
+    auto feedback = std::make_shared<AutoDig::Feedback>();
+    auto result = std::make_shared<AutoDig::Result>();
+
+    // If autodig fails, return goal aborted. Uncomment following:
+
+    // result->success = false;
+    // goal_handle->abort(result);
+    // RCLCPP_ERROR(this->get_logger(), "Autodig failed");
+    
+    // If autodig executed successfully, return goal success
+    if (rclcpp::ok()) {
+      result->success = true;
+      goal_handle->succeed(result);
+      RCLCPP_INFO(this->get_logger(), "Autodig succeeded");
+    }
+}
--- src/lx_autonomy/src/lx_packages/lx_operation/src/auto_nav_handler.cpp
+++ src/lx_autonomy/src/lx_packages/lx_operation/src/auto_nav_handler.cpp
@@ -93,3 +93,125 @@ void AutoNavHandler::paramCB(rclcpp::Client<rcl_interfaces::srv::GetParameters>:
         RCLCPP_INFO(this->get_logger(), "Service In-Progress...");
     }
 }
+
+void AutoNavHandler::setupCommunications(){
+    // Add all subscriptions, publishers and services here
+
+    // Clients
+    get_params_client_ = this->create_client<rcl_interfaces::srv::GetParameters>("/param_server_node/get_parameters");
+    // Action server
+    using namespace std::placeholders;
+    this->autonav_action_server_ = rclcpp_action::create_server<AutoNav>(this, "operations/autonav_action",
+                                            std::bind(&AutoNavHandler::handle_goal, this, _1, _2),
+                                            std::bind(&AutoNavHandler::handle_cancel, this, _1),
+                                            std::bind(&AutoNavHandler::handle_accepted, this, _1));
+}
+
+void AutoNavHandler::setupParams(){
+    // Subscriber for global parameter events
+    param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
+
+    // Callback Lambdas
+    auto mob_params_callback = [this](const rclcpp::Parameter & p) {
+        RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": %s", p.get_name().c_str(), (p.as_bool()?"Locked":"Unlocked"));
+        rover_soft_lock_.mobility_lock = p.as_bool();
+    };
+    auto act_params_callback = [this](const rclcpp::Parameter & p) {
+        RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": %s", p.get_name().c_str(), (p.as_bool()?"Locked":"Unlocked"));
+        rover_soft_lock_.actuation_lock = p.as_bool();
+    };
+    auto op_mode_params_callback = [this](const rclcpp::Parameter & p) {
+        switch(p.as_int()){
+            case 0:
+               current_rover_op_mode_ = OpModeEnum::STANDBY;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Standby", p.get_name().c_str());
+               break;
+            case 1:
+               current_rover_op_mode_ = OpModeEnum::TELEOP;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Teleop", p.get_name().c_str());
+               break;
+            case 2:
+               current_rover_op_mode_ = OpModeEnum::AUTONOMOUS;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Autonomous", p.get_name().c_str());
+               break;
+        }
+    };
+    auto task_mode_params_callback = [this](const rclcpp::Parameter & p){
+        switch(p.as_int()){
+            case 0:
+               current_rover_task_mode_ = TaskModeEnum::IDLE;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Idle", p.get_name().c_str());
+               break;
+            case 1:
+               current_rover_task_mode_ = TaskModeEnum::NAV;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Navigation", p.get_name().c_str());
+               break;
+            case 2:
+               current_rover_task_mode_ = TaskModeEnum::EXC;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Excavation", p.get_name().c_str());
+               break;
+            case 3:
+               current_rover_task_mode_ = TaskModeEnum::DMP;
+               RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": Dumping", p.get_name().c_str());
+               break;
+        }
+    };
+
+    // Names of node & params for adding callback
+    auto param_server_name = std::string("param_server_node");
+    auto mob_lock_param_name = std::string("rover.mobility_lock");
+    auto act_lock_param_name = std::string("rover.actuation_lock");
+    auto op_mode_param_name = std::string("rover.op_mode");
+    auto task_mode_param_name = std::string("rover.task_mode");
+
+    // Store callback handles for each parameter
+    mob_param_cb_handle_ = param_subscriber_->add_parameter_callback(mob_lock_param_name, mob_params_callback, param_server_name);
+    act_param_cb_handle_ = param_subscriber_->add_parameter_callback(act_lock_param_name, act_params_callback, param_server_name);
+    op_mode_param_cb_handle_ = param_subscriber_->add_parameter_callback(op_mode_param_name, op_mode_params_callback, param_server_name);
+    task_mode_param_cb_handle_ = param_subscriber_->add_parameter_callback(task_mode_param_name, task_mode_params_callback, param_server_name);
+}
+
+rclcpp_action::GoalResponse AutoNavHandler::handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const AutoNav::Goal> goal){
+    RCLCPP_INFO(this->get_logger(), "Received autonav request");
+    (void)uuid;
+    
+    // Accept and execute action
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse AutoNavHandler::handle_cancel(const std::shared_ptr<GoalHandleAutoNav> goal_handle){
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+    (void)goal_handle;
+    
+    // Cancel action
+    return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void AutoNavHandler::handle_accepted(const std::shared_ptr<GoalHandleAutoNav> goal_handle){
+    using namespace std::placeholders;
+    
+    // Start thread to execute action and detach
+    std::thread{std::bind(&AutoNavHandler::executeAutoNav, this, _1), goal_handle}.detach();
+}
+
+void AutoNavHandler::executeAutoNav(const std::shared_ptr<GoalHandleAutoNav> goal_handle){
+    RCLCPP_INFO(this->get_logger(), "Executing autonav request");
+    
+    // Get goal, feedback and result
+    const auto goal = goal_handle->get_goal();
+    auto feedback = std::make_shared<AutoNav::Feedback>();
+    auto result = std::make_shared<AutoNav::Result>();
+
+    // If autonav fails, return goal aborted. Uncomment following:
+    
+    // result->success = false;
+    // goal_handle->abort(result);
+    // RCLCPP_ERROR(this->get_logger(), "Autonav failed");
+    
+    // If autonav executed successfully, return goal success
+    if (rclcpp::ok()) {
+      result->success = true;
+      goal_handle->succeed(result);
+      RCLCPP_INFO(this->get_logger(), "Autonav succeeded");
+    }
+}
