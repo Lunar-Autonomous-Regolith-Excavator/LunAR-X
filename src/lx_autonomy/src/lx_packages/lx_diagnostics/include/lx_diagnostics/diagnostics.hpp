ba64fa659330516129bd33c474859bba7b0921ff
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-09-25 15:12:16 -0400

    Added diagnostics subscriber and set params

--- src/lx_autonomy/src/lx_packages/lx_diagnostics/include/lx_diagnostics/diagnostics.hpp
+++ src/lx_autonomy/src/lx_packages/lx_diagnostics/include/lx_diagnostics/diagnostics.hpp
@@ -2,21 +2,33 @@
 #define DIAGNOSTICS_H
 
 #include "rclcpp/rclcpp.hpp"
+#include "lx_msgs/msg/node_diagnostics.hpp"
+#include "rcl_interfaces/srv/get_parameters.hpp"
+#include "rcl_interfaces/srv/set_parameters.hpp"
+#include "rcl_interfaces/msg/parameter.hpp"
+#include "lx_library/lx_utils.hpp"
+#include <string>
 
 class Diagnostics: public rclcpp::Node
 {
     private:
         // Variables & pointers -----------------
+        std::vector<std::string> nodes_list_ {"param_server", 
+                                              "external_interface", 
+                                              "command_mux"};
         // Time
-        
+        unsigned int timeout_period_sec_ = 3;
+        rclcpp::TimerBase::SharedPtr diagnostics_timers_[3];
         // Subscribers
-
+        rclcpp::Subscription<lx_msgs::msg::NodeDiagnostics>::SharedPtr diagnostics_subscribers_[3];
         // Clients
-
-        // Publishers
-
+        rclcpp::Client<rcl_interfaces::srv::SetParameters>::SharedPtr set_params_client_;
+		rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedPtr get_params_client_;
         // Parameter handling
-
+        struct lock_struct rover_soft_lock_;
+        std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
+        std::shared_ptr<rclcpp::ParameterCallbackHandle> mob_param_cb_handle_;
+        std::shared_ptr<rclcpp::ParameterCallbackHandle> act_param_cb_handle_;
         // --------------------------------------
 
         // Functions ----------------------------
@@ -29,6 +41,26 @@ class Diagnostics: public rclcpp::Node
         * Set up tracking of global parameters
         * */
         void setupParams();
+
+        /*
+        * Get starting values of global parameters
+        * */
+        void getParams();
+
+        /*
+        * Callback function for starting values of global parameters
+        * */
+        void paramCB(rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedFuture );
+
+        /*
+        * Callback function for diagnostics
+        * */
+        void diagnosticsCallBack(const lx_msgs::msg::NodeDiagnostics::SharedPtr );
+
+        /*
+        * Set rover lock if diagnostics time out
+        * */
+        void setRoverLock();
         // --------------------------------------
 
     public:
--- src/lx_autonomy/src/lx_packages/lx_diagnostics/src/diagnostics.cpp
+++ src/lx_autonomy/src/lx_packages/lx_diagnostics/src/diagnostics.cpp
@@ -14,20 +14,111 @@ Diagnostics::Diagnostics(): Node("diagnostics_node"){
     // Set up subscriptions & publishers
     setupCommunications();
 
+    // Get parameters from the global parameter server
+    getParams();
+
     // Set up parameters from the global parameter server
     setupParams();
+
+    // Timer
+    for(long unsigned int i = 0; i < nodes_list_.size(); i++){
+        diagnostics_timers_[i] = this->create_wall_timer(std::chrono::seconds(timeout_period_sec_), std::bind(&Diagnostics::setRoverLock, this));
+    }
 
     RCLCPP_INFO(this->get_logger(), "Diagnostics initialized");
 }
 
 void Diagnostics::setupCommunications(){
     // Subscribers
-
-    // Publishers
-
+    for(long unsigned int i = 0; i < nodes_list_.size(); i++){
+        diagnostics_subscribers_[i] = this->create_subscription<lx_msgs::msg::NodeDiagnostics>("diagnostics/" + nodes_list_[i], 10, 
+                            std::bind(&Diagnostics::diagnosticsCallBack, this, std::placeholders::_1));
+    }
     // Clients
+    set_params_client_ = this->create_client<rcl_interfaces::srv::SetParameters>("/param_server_node/set_parameters");
+    get_params_client_ = this->create_client<rcl_interfaces::srv::GetParameters>("/param_server_node/get_parameters");
+}
+
+void Diagnostics::getParams(){
+    while(!get_params_client_->wait_for_service(std::chrono::seconds(2))){
+      RCLCPP_INFO(this->get_logger(), "Could not contact param server");
+      return;
+    }
+
+    // Get important parameters
+    auto get_request = std::make_shared<rcl_interfaces::srv::GetParameters::Request>();
+    get_request->names = {"rover.mobility_lock", "rover.actuation_lock"};
+
+    // Send request
+    auto param_result_ = get_params_client_->async_send_request(get_request,std::bind(&Diagnostics::paramCB, this, std::placeholders::_1));
+}
+
+void Diagnostics::paramCB(rclcpp::Client<rcl_interfaces::srv::GetParameters>::SharedFuture future){
+    auto status = future.wait_for(std::chrono::milliseconds(100));
+    // If request successful, save all params in global variables
+    if (status == std::future_status::ready) {
+        rover_soft_lock_.mobility_lock = future.get()->values.at(0).bool_value;
+        RCLCPP_DEBUG(this->get_logger(), "Parameter set Mobility: %s", (rover_soft_lock_.mobility_lock?"Locked":"Unlocked"));
+        rover_soft_lock_.actuation_lock = future.get()->values.at(1).bool_value;
+        RCLCPP_DEBUG(this->get_logger(), "Parameter set Actuation: %s", (rover_soft_lock_.actuation_lock?"Locked":"Unlocked"));
+    } 
+    else {
+        RCLCPP_INFO(this->get_logger(), "Service In-Progress...");
+    }
 }
 
 void Diagnostics::setupParams(){
-    // Parameter handling
+    // Subscriber for global parameter events
+    param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
+
+    // Callback Lambdas
+    auto mob_params_callback = [this](const rclcpp::Parameter & p) {
+        RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": %s", p.get_name().c_str(), (p.as_bool()?"Locked":"Unlocked"));
+        rover_soft_lock_.mobility_lock = p.as_bool();
+    };
+    auto act_params_callback = [this](const rclcpp::Parameter & p) {
+        RCLCPP_DEBUG(this->get_logger(), "Parameter updated \"%s\": %s", p.get_name().c_str(), (p.as_bool()?"Locked":"Unlocked"));
+        rover_soft_lock_.actuation_lock = p.as_bool();
+    };
+
+    // Names of node & params for adding callback
+    auto param_server_name = std::string("param_server_node");
+    auto mob_lock_param_name = std::string("rover.mobility_lock");
+    auto act_lock_param_name = std::string("rover.actuation_lock");
+
+    // Store callback handles for each parameter
+    mob_param_cb_handle_ = param_subscriber_->add_parameter_callback(mob_lock_param_name, mob_params_callback, param_server_name);
+    act_param_cb_handle_ = param_subscriber_->add_parameter_callback(act_lock_param_name, act_params_callback, param_server_name);
+}
+
+void Diagnostics::diagnosticsCallBack(const lx_msgs::msg::NodeDiagnostics::SharedPtr msg){
+    // Find the index of the node in the nodes_list_
+    int node_index = std::find(nodes_list_.begin(), nodes_list_.end(), msg->node_name) - nodes_list_.begin();
+    
+    // Respective timer reset
+    diagnostics_timers_[node_index]->reset();
+}
+
+void Diagnostics::setRoverLock(){
+    RCLCPP_ERROR(this->get_logger(), "DIAGNOSTICS TIMEOUT: A node has died");
+    if(!rover_soft_lock_.mobility_lock || !rover_soft_lock_.actuation_lock){
+        // Lock rover if some diagnostics message not received for timeout period
+        while(!set_params_client_->wait_for_service(std::chrono::seconds(1))){
+            RCLCPP_WARN(this->get_logger(), "Waiting for params server to be up...");
+        }
+
+        auto set_request = std::make_shared<rcl_interfaces::srv::SetParameters::Request>();
+        auto mob_param_req = rcl_interfaces::msg::Parameter();
+        mob_param_req.name = "rover.mobility_lock";
+        mob_param_req.value.type = 1;
+        mob_param_req.value.bool_value = true;
+        auto act_param_req = rcl_interfaces::msg::Parameter();
+        act_param_req.name = "rover.actuation_lock";
+        act_param_req.value.type = 1;
+        act_param_req.value.bool_value = true;
+
+        set_request->parameters = {mob_param_req, act_param_req};
+
+        auto future_result = set_params_client_->async_send_request(set_request);
+    }
 }
