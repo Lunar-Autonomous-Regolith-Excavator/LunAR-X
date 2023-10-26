4fd4aacfa057800fef9383282201b05935904a7b
Dhruv Tyagi <dhruv.tya@gmail.com>
2023-10-26 17:35:26 -0400

    Added drum_joint visualization, to be tested

--- src/lx_autonomy/src/lx_packages/lx_description/include/lx_description/joint_viz.hpp
+++ src/lx_autonomy/src/lx_packages/lx_description/include/lx_description/joint_viz.hpp
@@ -4,14 +4,17 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
+#include "lx_msgs/msg/tool_info.hpp"
+#include <cmath>
 
 class JointViz: public rclcpp::Node
 {
     private:
         // Variables & pointers -----------------
-        
+        const double twoPi = 2 * M_PI; // 2π
         // Subscribers
         rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr tool_height_subscriber_;
+        rclcpp::Subscription<lx_msgs::msg::ToolInfo>::SharedPtr tool_info_subscriber_;
         // Publishers
         rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_publisher_;
         // --------------------------------------
