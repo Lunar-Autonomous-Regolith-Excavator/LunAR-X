2cb9b783eaaa245ba783fab8095be509a2310853
Vibhakar Mohta <vib2810@gmail.com>
2023-10-27 20:08:26 -0400

    added imu orientation to TS to fix calib issue

--- src/lx_autonomy/src/lx_packages/lx_localization/src/remap_msgs_localization.cpp
+++ src/lx_autonomy/src/lx_packages/lx_localization/src/remap_msgs_localization.cpp
@@ -2,6 +2,8 @@
 #include "nav_msgs/msg/odometry.hpp"
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <geometry_msgs/msg/pose.h>
+#include <sensor_msgs/msg/imu.hpp>
+#include <geometry_msgs/msg/quaternion.hpp>
 
 // For tf2
 #include <tf2_ros/buffer.h>
@@ -18,22 +20,30 @@ public:
     {
         // Subscribe to the "odom" topic and remap it to "my_odom"
         auto qos = rclcpp::SensorDataQoS();
-        odom_sub_ = create_subscription<nav_msgs::msg::Odometry>("/husky_velocity_controller/odom", qos,
-                                        std::bind(&RemapNode::odom_callback, this, std::placeholders::_1));
-        odom_pub_ = create_publisher<nav_msgs::msg::Odometry>("/husky_odom", qos);
+        odom_sub_ = create_subscription<nav_msgs::msg::Odometry>("/husky_velocity_controller/odom", qos, std::bind(&RemapNode::odom_callback, this, std::placeholders::_1));
+        pose_sub_ = create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("/total_station_prism", qos, std::bind(&RemapNode::pose_callback, this, std::placeholders::_1));
+        imu_sub_ = create_subscription<sensor_msgs::msg::Imu >("/vectornav/imu", qos, std::bind(&RemapNode::imu_callback, this, std::placeholders::_1));
 
+        odom_pub_ = create_publisher<nav_msgs::msg::Odometry>("/husky_odom", qos);
         pose_pub_ = create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/total_station_pose_map", qos);
-        pose_sub_ = create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("/total_station_prism", qos,
-                                        std::bind(&RemapNode::pose_callback, this, std::placeholders::_1));
     }
 
 private:
     geometry_msgs::msg::TransformStamped eigen_transform_prism_baselink;
-    bool got_transform = false;
+    geometry_msgs::msg::TransformStamped eigen_transform_imu_baselink;
+    bool got_transforms = false;
+
+    // Subscribers and publishers
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
     rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
+
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
     rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_pub_;
+    // save imu orientation
+    geometry_msgs::msg::Quaternion imu_orientation;
+    bool got_imu = false;
+
     void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
         // Update the frame_id field in the message
@@ -48,9 +58,16 @@ private:
         odom_pub_->publish(*msg);
     }
 
+    void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+    {
+        got_imu = true;
+        this->imu_orientation = msg->orientation;
+    }
+
+    // Callback for Total Station Prism
     void pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
     {
-        if(this->got_transform == false)
+        if(this->got_transforms == false)
         {
             try 
             {
@@ -59,22 +76,34 @@ private:
                 tf2_ros::Buffer tf_buffer(this->get_clock());
                 tf2_ros::TransformListener tf_listener(tf_buffer);
                 this->eigen_transform_prism_baselink = tf_buffer.lookupTransform("base_link", "total_station_prism", tf2::TimePointZero, tf2::durationFromSec(0.5)); //primm to base link
-                this->got_transform = true;
+                this->eigen_transform_imu_baselink = tf_buffer.lookupTransform("base_link", "vectornav", tf2::TimePointZero, tf2::durationFromSec(0.5)); //primm to base link
+                this->got_transforms = true;
             } 
             catch (tf2::TransformException& ex) 
             {
-                std::cout<<"No transform found, publishing directly"<<std::endl;
-                msg->header.frame_id = "map";
-                pose_pub_->publish(*msg);
+                std::cout<<"No transforms found, please check if base_link to total_station_prism and base_link to vectornav are published"<<std::endl;
+                return;
             }
         }
-        if (this->got_transform == true)
+        if(this->got_imu == false)
+        {
+            std::cout<<"No IMU data found, please check if IMU is publishing"<<std::endl;
+            return;
+        }
+        if (this->got_transforms == true && this->got_imu == true)
         {   
             // std::cout<<"Transform found, publishing"<<std::endl;
             geometry_msgs::msg::PoseWithCovarianceStamped pose_map_msg;
+            sensor_msgs::msg::Imu imu_transformed;
             tf2::doTransform(*msg, pose_map_msg, this->eigen_transform_prism_baselink);
+            tf2::doTransform(this->imu_orientation, imu_transformed.orientation, this->eigen_transform_imu_baselink);
             pose_map_msg.header.frame_id = "map";
             pose_map_msg.header.stamp = msg->header.stamp;
+            // Add imu orientation to pose
+            pose_map_msg.pose.pose.orientation.x = imu_transformed.orientation.x;
+            pose_map_msg.pose.pose.orientation.y = imu_transformed.orientation.y;
+            pose_map_msg.pose.pose.orientation.z = imu_transformed.orientation.z;
+            pose_map_msg.pose.pose.orientation.w = imu_transformed.orientation.w;
             pose_pub_->publish(pose_map_msg);
         }
     }
