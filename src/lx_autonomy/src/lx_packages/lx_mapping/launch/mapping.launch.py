3b5581469412efbb31971b1ae6a7fad157a92184
anishrams <anishsenathi@gmail.com>
2023-11-11 23:51:07 -0500

    removed old moonyard_to_map static transform

--- src/lx_autonomy/src/lx_packages/lx_mapping/launch/mapping.launch.py
+++ src/lx_autonomy/src/lx_packages/lx_mapping/launch/mapping.launch.py
@@ -24,14 +24,6 @@ def generate_launch_description():
             emulate_tty=True
         )
         
-    tf_moonyard_link_old = Node(
-      package='tf2_ros',
-      executable='static_transform_publisher',
-      name='map_to_moonyard',
-      output='screen',
-      arguments=['-9', '-5' ,'0' ,'0' ,'0' ,'0', 'map', 'moonyard'],
-      emulate_tty=True
-    )
 
     tf_moonyard_link = Node(
       package='tf2_ros',
@@ -45,7 +37,6 @@ def generate_launch_description():
     ld = LaunchDescription()
     ld.add_action(pc_handler_node)
     ld.add_action(world_model_node)
-    # ld.add_action(tf_moonyard_link_old)
     ld.add_action(tf_moonyard_link)
     ld.add_action(auto_dump_visual_servoing_node)
 
